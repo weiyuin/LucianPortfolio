cdd90eb33df401c1f45724f416f9b712430face0
weiyuin <lucian.nguyen.it@gmail.com>
2026-06-08 00:09:56 +0530

    Update

--- assets/docs/code/H510脚本/椭圆点_574.py
+++ assets/docs/code/H510脚本/椭圆点_574.py
@@ -1,8 +1,8 @@
 points = GvVisionAssembly.sc2VectorVec()
 img = GvTool.GetToolData("旋转镜像工具_587.输出图像")
-startP =GvTool.GetToolData("找圆工具_027.圆心")
-for y in range(int(startP.GetY()),1000,1):
-    for x in range(int(startP.GetX()),1000,1):
-        if img.GetPixelValue(x,y) > 100:
-            points.append(GvVisionAssembly.sc2Vector(x,y))
-GvTool.SetToolData("椭圆拟合工具_575.输入点集",points)
+startP = GvTool.GetToolData("找圆工具_027.圆心")
+for y in range(int(startP.GetY()), 1000, 1):
+    for x in range(int(startP.GetX()), 1000, 1):
+        if img.GetPixelValue(x, y) > 100:
+            points.append(GvVisionAssembly.sc2Vector(x, y))
+GvTool.SetToolData("椭圆拟合工具_575.输入点集", points)
